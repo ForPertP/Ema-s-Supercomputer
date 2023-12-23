36f4ce700f6fdd2fe885af3708e28b9bd80ef058
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2023-12-24 05:51:47 +0900

    Update Ema-s-Supercomputer.cs

--- Ema-s-Supercomputer.cs
+++ Ema-s-Supercomputer.cs
@@ -11,3 +11,33 @@ using System.Runtime.Serialization;
 using System.Text.RegularExpressions;
 using System.Text;
 using System;
+
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+        int n = Convert.ToInt32(firstMultipleInput[0]);
+
+        int m = Convert.ToInt32(firstMultipleInput[1]);
+
+        List<string> grid = new List<string>();
+
+        for (int i = 0; i < n; i++)
+        {
+            string gridItem = Console.ReadLine();
+            grid.Add(gridItem);
+        }
+
+        int result = Result.twoPluses(grid);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
